b1e743866ccdc7ce0f92b4db422a067393238510
Renanppcosta <renanppcosta@gmail.com>
2023-02-10 16:39:58 -0300

    backend CRUD product created and page create product

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const Product = require('../controllers/productController');
+const upload = require("../middlewares/MulterMiddleware")
+
+
 
 const router = express.Router();
 
 router.get('/exibir', Product.exibir);
+router.get("/dashboard", Product.dashboardProduct);
+router.post("/create", upload.single("productionBanner"), Product.createProduct);
+router.get("/delete/:id", Product.deleteProduct);
+router.put("/update/:id", Product.updateProduct);
+router.get("/:id", Product.getProductById);
 
 module.exports = router;
