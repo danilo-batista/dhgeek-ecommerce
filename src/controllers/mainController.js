4fa23812ec71b7a56ec652b0b4cbd0bc7db8e4e3
Danilo Batista <dbatistadsn@gmail.com>
2023-02-11 18:46:09 -0300

    fix: relink an image that was broken.

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,8 @@
-const { getProducts } = require('../controllers/productController');
 
 const mainController = {
     home: async (req, res) => {
 
-        return res.render('../views/home', { getProducts })
+        return res.render('../views/home')
     },
 };
 
